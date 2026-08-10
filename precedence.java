384a6709425aee61088a6de746c8064cd6b6fb1e
DhrithiP <dhrithip017@gmail.com>
2026-08-10 11:23:21 +0530

    Create precedence.java

---
+++ precedence.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int m=10,n=5;
+        int res= n++ + m*m++ - 5;
+        //  5+10*10-5
+        System.out.println(res);
+       //output 100
+    }
+}
