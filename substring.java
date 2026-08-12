99d9720832f67b2bfdef0bd3e652c410d208d28c
DhrithiP <dhrithip017@gmail.com>
2026-08-12 09:59:07 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        String str="abc";
+        for(int i=0;i<str.length();i++){    
+            for(int j=i+1;j<=str.length();j++){
+                String sub=str.substring(i,j);
+                System.out.println(sub);
+            }
+        }
+    }
+}
+
+//output
+a
+ab
+abc
+b
+bc
+c
