300d844bb287c95f50337c7a7b47e77e2d70e2e1
DhrithiP <dhrithip017@gmail.com>
2026-08-15 09:33:53 +0530

    Create Permutation.java

---
+++ Permutation.java
@@ -0,0 +1,6 @@
+//1.PERMUTATIONS(String)
+//Given a String,print all permutations of the string using recursion.
+//Approach1:Fix one character,permute the rest
+//>pick one character >fix it at the current position >recursively permutue remaining characters
+//input: str="abc"
+//output: abc acb
