80edeccad1c5a12c1f3d165a5bff08b172829419
DhrithiP <dhrithip017@gmail.com>
2026-08-15 15:12:14 +0530

    Create House_problum.java

---
+++ House_problum.java
@@ -0,0 +1,6 @@
+//House Problum
+//Q: There are n houses build in a line,each of which contains some value in  it
+//A thief is going to steal the maximal value of these houses,but he can't steal in two adjesent houses because the owner of the stolen houses will tell his two neighbour left and right side.
+//what is the maximum stolen value?
+//Sample Input: val[]={6,7,1,3,8,2,5}
+//sample output: 20
