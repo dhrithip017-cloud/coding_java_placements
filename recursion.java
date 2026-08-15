54a3a6b5c1c6f0c1fd1563c47f673221c0858217
DhrithiP <dhrithip017@gmail.com>
2026-08-15 10:18:49 +0530

    Create recursion.java

---
+++ recursion.java
@@ -0,0 +1,31 @@
+//Printing name using REcursion(User input)
+import java.util.*;
+
+class main {
+
+    public static void printName(String name, int n) {
+        if (n < 0)
+            return;
+        else {
+            printName(name, n - 1);
+            char ch = name.charAt(n);
+            System.out.println(ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int n = name.length();
+
+        printName(name, n - 1);
+    }
+}
+//output: DHRITHI
+//D
+//H
+//R
+//I
+//T
+//H
+//I
